d1e5b52d577f4caa5bdd06a8969a542bd8c1ea9b
Julio Pedro López Bayón <jplb.fp@gmail.com>
2022-10-14 16:03:31 +0200

    confirmación de cambios

---
+++ src/main/java/ejercicios/unidad1/EjecutaSuma.java
@@ -0,0 +1,12 @@
+package ejercicios.unidad1;
+
+import java.io.IOException;
+
+public class EjecutaSuma {
+
+	public static void main(String[] args) throws IOException {
+		Process p = Runtime.getRuntime().exec("java");
+
+	}
+
+}
